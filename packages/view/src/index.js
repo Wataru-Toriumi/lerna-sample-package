59b89f925c06d37933c83154d39e1964a5d46651
Wataru-Toriumi <wataru.19930213@gmail.com>
2021-03-21 02:28:22 +0900

    feat: add some components.

--- packages/view/src/index.js
+++ packages/view/src/index.js
@@ -1,3 +1,15 @@
 import React from 'react';
 
+import {ButtonAppBar} from '@Wataru-Toriumi/header';
+import {TemporaryDrawer} from '@Wataru-Toriumi/drawer';
+
+export const SampleView = () => {
+  return (
+    <React.Fragment>
+      <ButtonAppBar />
+      <TemporaryDrawer />
+    </React.Fragment>
+  );
+};
+
 
